51afa7a982267cc63f6a1f4b8c796d4b8971562d
Artem Astapenko <jamakase54@gmail.com>
2020-06-24 14:57:59 +0700

    Test amplify passwordless flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import {Auth, Hub} from 'aws-amplify';
+import {useForm} from "react-hook-form";
+
 import './App.css';
 
-const App: React.FC = () => {
+import awsconfig from './aws-exports';
+
+Auth.configure(awsconfig);
+//
+// class MyCustomSignUp extends React.Component<ISignInProps, any> {
+//   gotoSignIn = () => {
+//     // to switch the authState to 'signIn'
+//     // @ts-ignore
+//     this.props.onStateChange('signIn', {});
+//   }
+//
+//   render() {
+//     return (
+//         <div>
+//           {/* only render this component when the authState is 'signUp' */}
+//           {this.props.authState === 'signUp' &&
+//           <div>
+//             My Custom SignUp Component
+//             <button onClick={this.gotoSignIn}>Goto SignIn</button>
+//           </div>
+//           }
+//         </div>
+//     );
+//   }
+// }
+
+const SignUp: React.FC<{}> = () => {
+  const {handleSubmit, register, errors} = useForm<{ phoneNumber: string }>();
+  const onSubmit = async ({phoneNumber}: { phoneNumber: string }) => {
+    try {
+      const result = await Auth.signUp({
+        username: phoneNumber,
+        password: "123456",
+        attributes: {
+          phone_number: phoneNumber
+        }
+      });
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="phoneNumber">
+          Phone
+        </label>
+        <input
+            name="phoneNumber"
+            ref={register({
+              required: 'Required',
+            })}
+        />
+        {errors.phoneNumber && errors.phoneNumber.message}
+        <button type="submit">Submit</button>
+      </form>
+  );
+};
+
+const SignIn: React.FC<{ setStatus: Function }> = ({setStatus}) => {
+  const {handleSubmit, register, errors, getValues} = useForm<{ phoneNumber: string, code: string }>();
+  const [requiresChallenge, setChallenge] = useState(false);
+  const [user, setUser] = useState(null);
+  const onSubmit = async ({phoneNumber, code}: { phoneNumber: string, code: string }) => {
+    console.log(requiresChallenge);
+    if (requiresChallenge) {
+      console.log(user, code);
+      try {
+        await Auth.sendCustomChallengeAnswer(user, code);
+        await Auth.currentSession();
+        setStatus("signedIn");
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const result = await Auth.signIn({
+          username: phoneNumber,
+          password: "",
+        });
+
+        setUser(result);
+
+        if (result.challengeName === "CUSTOM_CHALLENGE") {
+          setChallenge(true);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  const onResendCode = async () => {
+    const { phoneNumber } = getValues();
+    const result = await Auth.signIn({
+      username: phoneNumber,
+      password: "",
+    });
+
+    setUser(result);
+  }
+
+  return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="phoneNumber">
+          Phone
+        </label>
+        <input
+            name="phoneNumber"
+            ref={register({
+              required: 'Required',
+            })}
+        />
+        {errors.phoneNumber && errors.phoneNumber.message}
+        {requiresChallenge && <>
+          <label htmlFor="code">
+            Code
+          </label>
+          <input
+            name="code"
+            ref={register({
+              required: 'Required',
+            })}
+          />
+        </>}
+        <button type="submit">SignIn</button>
+        <button onClick={() => onResendCode()}>Resend code</button>
+      </form>
+  );
+};
+const App: React.FC = () => {
+  const [status, setStatus] = useState("offline");
+  return (
+      <div>
+        <SignUp/>
+        <SignIn setStatus={setStatus}/>
+        {status === "signedIn" && "SignedIn"}
+      </div>
   );
 }
 
